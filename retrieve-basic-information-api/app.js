cd72c77c472e62997a92ae09b5708f09ca17cf22
Matilda-Njau <matildawanjirunjau@gmail.com>
2025-02-05 17:25:07 +0300

    slight addition

--- retrieve-basic-information-api/app.js
+++ retrieve-basic-information-api/app.js
@@ -12,10 +12,17 @@ app.get("/", (req, res) => {
   res.status(200).json({
     "email": "njaumatildawanjiru@gmail.com",
     "current_datetime": new Date().toISOString().split(".")[0] + "Z",
-    "github_url": "https://github.com/njaumatilda/HNG-12__Backend-Development-Internship>"
+    "github_url": "<https://github.com/njaumatilda/HNG-12__Backend-Development-Internship>"
 })
 })
 
-app.listen(SERVER_PORT, () => {
-  console.log(`[server]: App listening on port: ${SERVER_PORT}`)
-})
+// app.listen(SERVER_PORT, () => {
+//   console.log(`[server]: App listening on port: ${SERVER_PORT}`)
+// })
+
+
+export default app
+
+// Before deploying, ensure to:
+//  + make changes to the app.js, package.json and add a vercel.json
+//  + also. remember to add environment variables on vercel
